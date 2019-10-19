d15340b9a93ab166e6b2020bcb3f6862119c1cd1
Perrin Antonin <tom.per@orange.fr>
2019-10-19 18:23:50 +0200

    tab user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -33,6 +33,41 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $postalcode;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $creationdate;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $changedate;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -104,5 +139,89 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getPostalcode(): ?int
+    {
+        return $this->postalcode;
+    }
+
+    public function setPostalcode(?int $postalcode): self
+    {
+        $this->postalcode = $postalcode;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreationdate(): ?\DateTimeInterface
+    {
+        return $this->creationdate;
+    }
+
+    public function setCreationdate(\DateTimeInterface $creationdate): self
+    {
+        $this->creationdate = $creationdate;
+
+        return $this;
+    }
+
+    public function getChangedate(): ?\DateTimeInterface
+    {
+        return $this->changedate;
+    }
+
+    public function setChangedate(\DateTimeInterface $changedate): self
+    {
+        $this->changedate = $changedate;
+
+        return $this;
     }
 }
