4fd1626d812b70d5dcc6b61d5d982b049e4f510c
Perrin Antonin <tom.per@orange.fr>
2019-10-19 22:00:08 +0200

    css form

--- src/Form/CreateAccountFormType.php
+++ src/Form/CreateAccountFormType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +18,20 @@ class CreateAccountFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
+            ->add('username' , TextType::class,['attr'=> ['class'=> 'form-control',"placeholder"=>"Username"],])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                    'required' => true,
+                    'first_options'  => ['attr'=> ['class'=> 'form-control',"placeholder"=>"MDP"],"label"=>false],
+                    'second_options' => ['attr'=> ['class'=> 'form-control',"placeholder"=>"Repeat MDP"],"label"=>false],])
 
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('postalcode')
-            ->add('address')
-            ->add('description')
-            ->add('creationdate')
-            ->add('changedate')
+            ->add('firstname', TextType::class,['attr'=> ['class'=> 'form-control',"placeholder"=>"Prénom"],])
+            ->add('lastname', TextType::class,['attr'=> ['class'=> 'form-control',"placeholder"=>"Nom"],])
+            ->add('address', TextType::class,['attr'=> ['class'=> 'form-control',"placeholder"=>"Adresse"],])
+            ->add('city', TextType::class,['attr'=> ['class'=> 'form-control',"placeholder"=>"Ville"],])
+            ->add('postalcode', IntegerType::class,['attr'=> ['class'=> 'form-control',"placeholder"=>"Code Postal "],] )
+
+            ->add('description',TextareaType::class,['attr' =>['class'=> 'form-control w-100',"placeholder"=>"Leave a description about you", "cols"=>"30", "rows"=>"10"],])
         ;
     }
 
